f43a4c499ae130c7647c1c078813135e65883994
First Repo <bharatrajput8287@gmail.com>
2024-09-09 10:07:46 +0530

    Learning about variables and constants

---
+++ variables and constants.js
@@ -0,0 +1,28 @@
+const id = 2100320100053;
+let Name = "Bharat";
+var email = "bharatrajput8287@gmail.com";
+city = "Ghaziabad";  // bad practice
+let state; 
+
+
+// id = 2100320100049;  not allowed
+// console.log(id);
+
+Name = "Bharat Chauhan";
+
+
+email = "bharatchauhan8287@gmail.com";
+
+
+city = "Loni";
+
+
+console.table([id, Name, email, city, state]);
+
+/* 
+prefer not to use var
+because of issue in block scope and function scope
+*/
+
+ 
+
