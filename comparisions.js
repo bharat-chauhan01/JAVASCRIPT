b98587252b5193f624e55ec8a34d87f074e69a2f
First Repo <bharatrajput8287@gmail.com>
2024-11-04 09:30:15 +0530

    comparisions of data types

---
+++ comparisions.js
@@ -0,0 +1,26 @@
+// Basic compariosions
+
+// console.log(2 == 3);
+// console.log(2 > 3);
+// console.log(2 < 3);
+// console.log(2 >= 3);
+// console.log(2 <= 3);
+// console.log(2 != 3);
+
+
+console.log(null > 0);
+console.log(null == 0);   // In JS, Equality and Comparisions(treat null as 0) works differently
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);   // So, avoid these type of comparisions
+console.log(undefined < 0);
+
+// Normal equality check
+console.log("2" == 2);
+
+// Strict check
+console.log("2" === 2); // It checks the data types as well
+
+
+
